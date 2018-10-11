58ec7d50096c4cdfbe1a7d143c99dba9b72f8a9f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 12:15:59 -0400

    Saving map_viz.R

--- map_viz.R
+++ map_viz.R
@@ -1,5 +1,4 @@
 
 library(ggmap)
 library(ggplot2)
-usmap<-get_map(location="us",source=c("osm"))
-viz=ggmap(usmap)+ggplot(data)+aes(x=stateCenter.x,y=stateCenter.y,color=stateArea)
+viz=ggplot(data)+geom_map(map=us,fill="white",color="black")+aes(x=stateCenter.x,y=stateCenter.y,color=stateArea)
