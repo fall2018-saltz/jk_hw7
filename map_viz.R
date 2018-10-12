e492b67306e33441bb526e17d966c9bbca3605f4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-12 11:57:49 -0400

    Saving map_viz.R

--- map_viz.R
+++ map_viz.R
@@ -3,5 +3,4 @@ library(ggmap)
 library(ggplot2)
 library(maps)
 usa <- map_data("usa")
-data
-viz=ggplot(data)+geom_polygon(data=usa,fill="white",color="black")+aes(x=stateCenter.x,y=stateCenter.y,color=stateArea)
+viz=ggplot(data)+geom_polygon(data=usa,fill="white",color="black")+aes(x=data$stateCenter.x,y=data$stateCenter.y,color=data$stateArea)
