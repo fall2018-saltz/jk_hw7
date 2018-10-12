a8f3f3b5a3bcb91b15b42f996ce826f39bffd81b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-12 12:43:41 -0400

    Saving map_viz.R

--- map_viz.R
+++ map_viz.R
@@ -10,3 +10,12 @@ viz <- ggplot(data, aes(map_id=stateName)) + geom_map(map=us, aes(fill=stateArea
 vizmurder <- ggplot(data, aes(map_id=stateName)) + geom_map(map=us, aes(fill=Murder))+ expand_limits(x=us$long,y=us$lat) + coord_map() + ggtitle("US Map Murder Rates")
 vizpopulation<-viz+geom_point(aes(x=stateCenter.x,y=stateCenter.y,size=population))
 
+nycx <- NewMergeDf[NewMergeDf$stateName=="new york","x"]
+nycy <- NewMergeDf[NewMergeDf$stateName=="new york","y"]
+
+viznyc <- ggplot(data, aes(map_id=stateName)) +
+  geom_map(map=us, aes(fill=Statearea))+
+  geom_point(aes(x=stateCenter.x,y=stateCenter.y,size=Population))+
+  expand_limits(x=us$long,y=us$lat) + 
+  coord_map(xlim=c(nycx+10,nycx-10),ylim=c(nycy+10,nycy-10))+
+  ggtitle("Us North Eastern Map Area with population")
