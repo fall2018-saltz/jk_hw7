fb67fe727ef0ece113691dc80e2a595dd6a7fa9f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-12 12:20:34 -0400

    Saving map_viz.R

--- map_viz.R
+++ map_viz.R
@@ -3,7 +3,6 @@ library(ggplot2)
 library(ggmap)
 library(maps)
 us <- map_data("state")
-View(us)
 #Step B: Generate a color coded map
 viz <- ggplot(NewMergeDf, aes(map_id=stateName)) +
            geom_map(map=us, aes(fill=NewMergeDf$area))+
