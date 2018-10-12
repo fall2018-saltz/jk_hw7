b0534262e7575844f422a6bc04ef2227ade984bb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-12 12:24:13 -0400

    Saving map_viz.R

--- map_viz.R
+++ map_viz.R
@@ -6,6 +6,6 @@ data$stateName<-tolower(data$stateName)
 us <- map_data("state")
 #Step B: Generate a color coded map
 viz <- ggplot(data, aes(map_id=stateName)) +
-           geom_map(map=us, aes(fill=data$stateArea))+
+           geom_map(map=us, aes(fill=stateArea))+
            expand_limits(x=us$long,y=us$lat) + 
            coord_map() + ggtitle("Us Map Area")
