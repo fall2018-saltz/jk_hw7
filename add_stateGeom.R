0d953a7e14316653cb6c3b12b77cd0bd5193808e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 20:43:30 -0400

    Saving add_stateGeom.R

--- add_stateGeom.R
+++ add_stateGeom.R
@@ -1,4 +1,4 @@
 
+#making a new data frame that consists of state names, state centers and area
 new_data=data.frame(stateName=state.name,stateCenter=state.center,stateArea=state.area,stringsAsFactors=FALSE)
 data<-merge(data,new_data,by='stateName',all=TRUE)
-data
