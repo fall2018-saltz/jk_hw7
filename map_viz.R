1c54687315813dc1a986bb6fd20307fc09483a02
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 20:33:58 -0400

    Saving map_viz.R

--- map_viz.R
+++ map_viz.R
@@ -8,6 +8,7 @@ us <- map_data("state")
 #Step B: Generate a color coded map
 viz <- ggplot(data, aes(map_id=stateName)) + geom_map(map=us, aes(fill=stateArea))+ expand_limits(x=us$long,y=us$lat) + coord_map() + ggtitle("US Map")
 
+#Step C: Create a color shaded map of the U.S. based on the Murder rate for each state
 vizmurder <- ggplot(data, aes(map_id=stateName)) + geom_map(map=us, aes(fill=Murder))+ expand_limits(x=us$long,y=us$lat) + coord_map() + ggtitle("US Map Murder Rates")
 vizpopulation<-viz+geom_point(aes(x=stateCenter.x,y=stateCenter.y,size=population))
 
